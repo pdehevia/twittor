77888986e0d7194eeb1edd1cdb99ec0fde0ef3f8
Pablo <pdehevia@gmail.com>
2021-09-08 16:09:51 +0200

    first commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+
+function actualizaCacheDinamico( dynamicCache, request, response ) {
+    if( response.ok ){
+        caches.open(dynamicCache).then( cache => {
+            cache.put(request, response.clone() );
+            return response.clone();
+        });
+    } else {
+        return response;
+    }
+}
