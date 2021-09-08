993d5f01302a0958b59da1db98b3e9362bb250cc
Pablo <pdehevia@gmail.com>
2021-09-08 16:48:11 +0200

    Modificaciones iOS

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ if( 'function' === typeof importScripts) {
  }
 
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
